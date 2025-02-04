a41c27f099ddc3f53a4dd08884e6180d005cd9ad
victoriamelnikova <maroon.light13@gmail.com>
2025-02-04 22:44:31 +0300

    fix 3

--- app/main.py
+++ app/main.py
@@ -26,4 +26,4 @@ app.include_router(users.router)
 
 if __name__ == "__main__":
     # create_db_and_tables()
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="localhost", port=8002)
